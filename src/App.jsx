6909c783028f2af6af1ad1488a652aac3cf9a190
Smethan <ethanhargus@gmail.com>
2022-12-14 14:21:08 -0600

    Main project structure

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./containers/Main/Main";
+import Navbar from "./containers/Navbar/Navbar";
+import { useState } from "react";
+import beers from "./beers";
+import { useEffect } from "react";
+
+function App() {
+	const [beerList, setBeerList] = useState([]);
+
+	useEffect(() => {
+		setBeerList(beers);
+	}, []);
+
+	return (
+		<div className="App">
+			<Navbar setBeerList={setBeerList} />
+			<Main beerList={beerList} />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/containers/Main/Main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import beers from "../../beers";
+import { useState, useEffect } from "react";
+
+const Main = () => {
+	return <div>Main</div>;
+};
+
+export default Main;
